7f024f5325032cd566b67b6cc2c3e67ed328b0a9
bluealphanumeric9 <92750489+bluealphanumeric9@users.noreply.github.com>
2022-02-03 20:53:24 +0400

    Add files via upload
    This is the same as task 3, just to show that group by one column is enough.

---
+++ Task3a.sql
@@ -0,0 +1,17 @@
+-- Write a query returning a highest pay salary 
+
+use employees;
+select
+	employees.first_name,
+    employees.last_name,
+    dept_emp.dept_no,
+    departments.dept_name,
+    salaries.salary,
+    max(salaries.salary) as max_salary
+
+
+from employees
+ join dept_emp on employees.emp_no = dept_emp.emp_no
+ join  departments on dept_emp.dept_no = departments.dept_no
+ join salaries on employees.emp_no = salaries.emp_no 
+ group by departments.dept_name 
